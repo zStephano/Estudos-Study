fab44831aa5c6cc6c6cd198c89a1c3d0c86673dc
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-06-12 18:23:22 -0300

    Add more knowledge: + Two pointer + reverse; + Cases to study; + Anotations + Directory organization.

---
+++ C#/Projetos de Estudo/Study/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+
+string inputOne = "###$$$1234*";
+string inputTwo = "abc@12#ef$";
+Console.WriteLine(ReverseString(inputOne));
+Console.WriteLine(ReverseString(inputTwo));
+
+static string ReverseString(string data)
+{
+    char [] chars = data.ToCharArray();
+    int leftPointer = 0, rightPointer = data.Length - 1;
+    while(leftPointer < rightPointer)
+    {
+        if (Char.IsLetterOrDigit(chars[leftPointer]))
+        {
+            if (Char.IsLetterOrDigit(chars[rightPointer]))
+            {
+                char temp = chars[leftPointer];
+                chars[leftPointer] = chars[rightPointer];
+                chars[rightPointer] = temp;
+                leftPointer++;
+                rightPointer--;
+            }
+            else
+            {
+                rightPointer--;
+            }
+        }
+        else
+        {
+            leftPointer++;
+        }
+    }
+    return new string(chars);
+}
