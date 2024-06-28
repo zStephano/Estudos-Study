15cf63f813e684361160bd79e15058e16cf31bd7
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-06-27 23:10:25 -0300

    + Study

--- C#/Projetos de Estudo/Fundamentos da Orientação a Objetos/OopStudy/OopStudy/SubscriptionContext/Student.cs
+++ C#/Projetos de Estudo/Fundamentos da Orientação a Objetos/OopStudy/OopStudy/SubscriptionContext/Student.cs
@@ -1,4 +1,5 @@
-﻿using OopStudy.SharedContext;
+﻿using OopStudy.NotificationContext;
+using OopStudy.SharedContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,24 @@ namespace OopStudy.SubscriptionContext
 {
     public class Student : Base
     {
+        public Student() 
+        {
+            Subscriptions = new List<Subscription>();
+        }
         public string Name { get; set; }
         public string Email { get; set; }
         public User User { get; set; }
         public IList<Subscription> Subscriptions { get; set; }
+        public void CreateSubscription(Subscription subscription)
+        {
+            if(IsPremium)
+            {
+                AddNotification(new Notification("Premium", "O Aluno já tem uma assinatura ativa"));
+                return;
+            }
+
+            Subscriptions.Add(subscription);
+        }
         public bool IsPremium => Subscriptions.Any(x => !x.IsInactive);
     }
 }
