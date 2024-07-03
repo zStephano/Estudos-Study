72f5612f3839e544ad985f35880741e97e3d1b04
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-07-03 15:27:01 -0300

    + Study, leetcode, context, notes, knowledge

--- Linguagens de Programação/C#/Projetos de Estudo/Fundamentos do C#/ConditionsHandlers/obj/Debug/net8.0/ConditionsHandlers.AssemblyInfo.cs
+++ Linguagens de Programação/C#/Projetos de Estudo/Fundamentos do C#/ConditionsHandlers/obj/Debug/net8.0/ConditionsHandlers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConditionsHandlers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2408300d076b3be5edf339d5d70bbd49c0096c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca8fece6e1d283653dc31dadf10cefdcd365157")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConditionsHandlers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConditionsHandlers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Linguagens de Programação/C#/Projetos de Estudo/Fundamentos do C#/LimitValidationCount/Program.cs
@@ -0,0 +1,32 @@
+﻿// Input dos dados
+int[] inputArray = [1, 5, 2, 8];
+int[] limitsArray = [2, 5, 4];
+
+// Impressão dos dados através do retorno do método LimitValidationCount
+int[] output = LimitValidationCount(inputArray, limitsArray);
+Console.WriteLine(string.Join(", ", output)); // Output: 2, 3, 2
+
+int[] LimitValidationCount(int[] inputArray, int[] Limits)
+{
+    // Intanciamento do result com o tamanho do Limit
+    var result = new int[Limits.Length];
+
+    // Ordenação da matriz A, ficando [1, 2, 5, 8]
+    Array.Sort(inputArray);
+
+    // Enquanto o i for menor que o tamanho da matriz Limits
+    for (int i = 0; i < Limits.Length; i++)
+    {
+        // Variável limit receberá o valor da posição "i" da matriz Limit
+        int limit = Limits[i];
+
+        // A variável count irá percorrer a matriz A enquanto cada valor de A for menor igual ao valor da variável Limit
+        int count = inputArray.TakeWhile(a => a <= limit).Count();
+        
+        // Quando a regra da variável count for atiginda, irá adicionar este valor a posição correspondente de "i"
+        result[i] = count;
+    }
+
+    // Ao final do método, retornará uma nova matriz correspondendo a conta dos números que são menores de acordo com cada valor do limit
+    return result;
+}
